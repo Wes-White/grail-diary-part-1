ff6b5de4f2db12d99a8e3a27f466e67db04558fc
Wesley White <wesleyrwhite86@gmail.com>
2020-03-17 08:41:40 -1000

    Added new project Grail Diary

---
+++ Grail Diary/Grail Diary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Grail Diary
+//
+//  Created by Wesley Ryan on 3/17/20.
+//  Copyright © 2020 Wesley Ryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
